95c20f81723fc5aa8ee2e644d82366865d8d7863
jha <j.haimez@vattenfall.fr>
2025-04-17 08:18:59 +0200

    feat!: change name function, add color, change emoji
    BC

--- release.config.js
+++ release.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { breaking: true, release: "major" }, // important !
+        ],
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BC"],
+        },
+      },
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     ['@semantic-release/github', {
